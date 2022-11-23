42766a706eabba311657cb5fa4d41f2dccc96cf9
McRonaah <ronaldcheruiyot342@gmail.com>
2022-11-24 00:01:53 +0300

    index

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,2 +1,14 @@
 class ProductionsController < ApplicationController
+    def index
+        render json:Production.all,status: :ok
+    end
+    def show
+    byebug
+        production = Production.find_by(id:params[:id])
+        if production
+            render json:production,status: :okay
+        else
+            render json: {error:"production not found"}, status: :not_found
+        end
+    end
 end
